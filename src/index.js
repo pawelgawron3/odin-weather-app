8b2034aa4461c8a8715f247e375b4fe8ceae6e2d
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-22 13:21:33 +0200

    Add event listener, import 2 .js modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+import { fetchData } from "./fetchData";
+import { handleEmptyInput } from "./handleEmptyInput";
+
+const searchBtn = document.querySelector("#searchBtn");
+
+searchBtn.addEventListener("click", () => {
+  let city = document.querySelector("#cityInput").value.trim();
+  if (city !== "") {
+    let json = fetchData(city);
+  } else {
+    handleEmptyInput();
+  }
+});
