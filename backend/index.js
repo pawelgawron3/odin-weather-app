35c3601ee279d41f3b8c9e48294452f3db67e48b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-22 18:59:19 +0200

    Use cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 // GET /weather?city=xxx
 app.get("/weather", async (req, res) => {
