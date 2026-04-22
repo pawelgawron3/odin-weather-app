fa4e3027c9b3254cafcbf7dc6a74986d0454b633
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-22 16:50:57 +0200

    Refactor main file code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,19 @@
 import "./styles.css";
-import { fetchData } from "./fetchData";
-import { handleInputError } from "./handleInputError";
-import { displayData } from "./displayData";
+import handleSearch from "./handleSearch";
 
-let temp = false; //false => Fahrenheit
+let temp = false; // false => Fahrenheit
 
 const cityInput = document.querySelector("#cityInput");
 const searchBtn = document.querySelector("#searchBtn");
 const tempToggle = document.querySelector("#tempToggle");
 
 tempToggle.addEventListener("click", () => {
-  temp = temp ? false : true;
-  tempToggle.textContent = tempToggle.textContent == "°F" ? "°C" : "°F";
+  temp = !temp;
+  tempToggle.textContent = tempToggle.textContent === "°F" ? "°C" : "°F";
 });
 
-searchBtn.addEventListener("click", async () => {
-  let city = cityInput.value.trim();
-  if (city !== "") {
-    let json = await fetchData(city);
-    if (json) {
-      displayData(json, temp);
-    } else {
-      handleInputError();
-    }
-    cityInput.value = "";
-  } else {
-    handleInputError();
-  }
-});
+searchBtn.addEventListener("click", () => handleSearch(temp, cityInput));
 
-cityInput.addEventListener("keydown", async (e) => {
-  if (e.key === "Enter") {
-    searchBtn.click();
-  }
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") handleSearch(temp, cityInput);
 });
