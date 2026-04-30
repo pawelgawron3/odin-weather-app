7ac9d7092a30c131c55b2a66b73420a281a7837c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-30 15:02:02 +0200

    Import spinner functions, refactor handleSearch() logic

--- frontend/src/handleSearch.js
+++ frontend/src/handleSearch.js
@@ -1,6 +1,7 @@
 import fetchData from "./fetchData";
 import handleInputError from "./errorHandlers/handleInputError";
 import handleNetworkError from "./errorHandlers/handleNetworkError";
+import { showLoader, hideLoader } from "./spinner";
 import { displayData } from "./displayData";
 
 export default async function handleSearch(temp, cityInput) {
@@ -11,18 +12,17 @@ export default async function handleSearch(temp, cityInput) {
     return;
   }
 
+  showLoader();
   const data = await fetchData(city);
 
-  if (data.error === "network") {
+  if (data?.error === "network") {
     handleNetworkError();
-    return;
-  }
-
-  if (data.error === "api") {
+  } else if (data?.error === "api") {
     handleInputError();
-    return;
+  } else {
+    displayData(data, temp);
   }
 
-  displayData(data, temp);
+  hideLoader();
   cityInput.value = "";
 }
