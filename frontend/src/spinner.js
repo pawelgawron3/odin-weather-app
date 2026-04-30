d18d02817d2086282ee6655684ee05bcdf796dbb
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-04-30 14:59:04 +0200

    Create spinner.js

---
+++ frontend/src/spinner.js
@@ -0,0 +1,21 @@
+const loader = document.querySelector("#loader");
+const emojis = ["🌤️", "⛅", "🌧️", "🌩️"];
+let emojiInterval;
+
+function showLoader() {
+  const emojiEl = document.querySelector("#weatherEmoji");
+  loader.classList.remove("hidden");
+
+  let i = 0;
+  emojiInterval = setInterval(() => {
+    emojiEl.textContent = emojis[i % emojis.length];
+    i++;
+  }, 500);
+}
+
+function hideLoader() {
+  loader.classList.add("hidden");
+  clearInterval(emojiInterval);
+}
+
+export { showLoader, hideLoader };
